0f244d90bcef7fbf197107c51b49df9498b5dc85
lee heechan <leeheechan1@gmail.com>
2026-07-14 01:23:28 +0900

    2026-07-14 변경 사항
    2026-05월 static-web-sre
    Github: 불필요한 테스트 제거
    Terraform: 경로 지정을 변수를 활용해 다시 해봄

--- aws/2026/05/static-web-sre/src/environments/dev/variables.tf
+++ aws/2026/05/static-web-sre/src/environments/dev/variables.tf
@@ -4,7 +4,7 @@ variable "region" {
 }
 
 # S3 #
-# Bucket : To Trigger Github Action 8
+# Bucket : To Trigger Github Action 9
 variable "s3_name" {
   type        = string
   description = "AWS S3의 이름"
