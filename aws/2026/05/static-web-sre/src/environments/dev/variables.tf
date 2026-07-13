4915b6bc39d2142d88a534403c052fd13540fccf
lee heechan <leeheechan1@gmail.com>
2026-07-14 00:12:32 +0900

    2026-07-14 변경 사항
    2026-05월 static-web-sre
    Github: 테스트 추가
    Terraform: 주석 변경. Github Action 시작

--- aws/2026/05/static-web-sre/src/environments/dev/variables.tf
+++ aws/2026/05/static-web-sre/src/environments/dev/variables.tf
@@ -4,7 +4,7 @@ variable "region" {
 }
 
 # S3 #
-# Bucket : To Trigger Github Action 2
+# Bucket : To Trigger Github Action 3
 variable "s3_name" {
   type        = string
   description = "AWS S3의 이름"
